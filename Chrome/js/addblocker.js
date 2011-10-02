d62cea59dc1fa088745b3ffcf005eefc47b8f2d5
Sara Chipps <sarajchipps@Sara-Chippss-MacBook-Pro.local>
2011-10-02 14:48:05 -0400

    Added Background

--- Chrome/js/addblocker.js
+++ Chrome/js/addblocker.js
@@ -6,12 +6,14 @@ t[h]}if(f.isEmptyObject(t)){var u=s.handle;u&&(u.elem=null),delete s.events,dele
 addblocker = {};
 addblocker.inserted = false;
 addblocker.number_alerts;
-addblocker.frame;         
+addblocker.frame;  
+addblocker.number_notifications = 0;
+       
 
 
 //DISABLE THIS WHEN TROBLESHOOTING
-//function noError(){return true;}
-//window.onerror = noError;
+function noError(){return true;}
+window.onerror = noError;
 
 
 
@@ -24,152 +26,51 @@ addblocker.findCanvasFrame = function(){
 	return a
 }
 
-
-
-
 //AFTER DOCUMENT LOAD
-addblocker.insertPlugin = function(){
-	getNotifications();
-};
-
-
 $(function() {
-	setInterval(addblocker.insertPlugin, 1000);
+	addblocker.getNotifications();
+	setInterval(addblocker.getNotifications, 10000);
+	setInterval(addblocker.updateAlert, 500);
 });	
 
 
-function checkStatuses(statuses){
-	var frame = addblocker.findCanvasFrame();
-	var number_of_notifications = 0;
+addblocker.checkStatuses = function(statuses){
+	addblocker.number_notifications = 0;
 	//console.log("here are the original number of notifications", statuses);
 	for(i =0; i<statuses.length; i++){
 		if(statuses[i].unread){
-			number_of_notifications++;
+			addblocker.number_notifications++;
 			if(statuses[i].html.indexOf("added you") > 0)
 			{
-				number_of_notifications--;
+				addblocker.number_notifications--;
 			}
 		}
 	}
 	
-	console.log("number of notifications: ", number_of_notifications);
-	$("#gbi1", frame).text(number_of_notifications);	
-    if(number_of_notifications == 0)
+	
+}
+
+addblocker.updateAlert = function(){
+	var frame = addblocker.findCanvasFrame();
+	console.log("number of notifications: ", addblocker.number_notifications);
+	$("#gbi1", frame).text(addblocker.number_notifications);	
+    if(addblocker.number_notifications == 0)
 	{
-		$("#gbi1a", frame).css("background", "//ssl.gstatic.com/gb/images/h_bedf916a.png").css("background-position", "0");
+		$("#gbi1a", frame).css("background", "").css("background-position", "").css("background-color", "#708090");
  	}
+	else{
+		$("#gbi1a", frame).css("background", "").css("background-position", "").css("background-color", "red");
+	}
 }
 
 
-function getNotifications(){
-  
-      var xhr = new XMLHttpRequest();
-      xhr.open('get', 'https://plus.google.com/u/0/_/notifications/getnotificationsdata', true);
-      xhr.onload = function(){
-        var note =  parseNotifications(eval(xhr.responseText.substr(5)));
-        //console.log("this is note", note);
-		checkStatuses(note);
-       
-        }
-      xhr.send();
+addblocker.getNotifications = function(){
+	chrome.extension.sendRequest({message: "xhr"}, function(response) {
+	  addblocker.checkStatuses(response.statuses);
+	});     
  }
 
-//this function is from the surplus chrome plugin, authored by
-function parseNotifications(x){
-  var verbs = { //
-    6: 'added you on Google+',
-    32: 'invited you to join Google+',
-    1: 'wrote on your profile',
-    24: 'shared a {thing} with you',
-    2: 'commented on your {thing}',
-    3: 'commented on a {thing} you commented on',
-    4: 'liked your {thing}',
-    20: "+1'd your {thing}",
-    21: "+1'd your comment on a {thing}",
-    16: 'mentioned you in a {thing}',
-    15: 'mentioned you in a comment on a {thing}',
-    14: 'commented on a {thing} that you were mentioned in',
-    5: 'reshared your {thing}',
-    10: 'tagged you in a photo',
-    13: 'tagged your photo',
-    25: "commented on a {thing} you're tagged in",
-    26: "commented on a {thing} you tagged",
-    29: "invited you to a new conversation on Google+ Mobile",
-    33: "invites you to a hangout"
-  }
-
-var last = x[1][1]; //or is it 2?!?!?!? 
-//2 seems to be the last time that updatelastreadtime was called
-return x[1][0].map(function(e){ //loop through every notification
-//  console.log(e);
-  var notifyType = e[6]; //2 = photo, 1 = post
-
-  var thingurl = e[7]?('https://plus.google.com/u/'+(localStorage.auth_user||0)+'/'+e[7][21]):null;
-  var thing = (notifyType == 2 ? 'photo' : 'post');
-  thing = '<a href="'+thingurl+'" target=_blank>'+thing+'</a>';
-  var actlist = [];
-  var actions = e[2].map(function(e){ //iterate through the participants
-    var code = e[1][0][1]; //retrieve the action code of the first participant in the list
-    var actors = e[1].map(function(k){
-      var profile = 'https://profiles.google.com/'+k[2][1];
-      var pic = k[2][2];
-      actlist.push(k);
-      return '<a href="'+profile+'" target=_blank '+((k[3]-last>0)?'style="font-weight:bold"':'')+'>'+k[2][0]+'</a>';
-      return k[2][0]
-    });
-    var main = actors.join(', ');
-    if(actors.length > 4){
-      main = actors.slice(0, 3).join(', ') + ', and '+(actors.length - 3)+' others'
-    }
-    
-    return main +' '+(verbs[code]||('did something ('+code+')'));
-  });
-  var actpic = actlist.sort(function(a, b){
-    return b[3] - a[3]
-  })[0][2][2];
-  var act = actions[0];
-  if(actions.length > 1){
-    act += (
-      ((actions.length > 2)?(', ' + actions.slice(1, -1).join(', ')):'')
-      + ', and ' + actions.slice(-1))
-    .replace(/(your|a) \{thing\}/g, 'it') //uses pronouns
-  }
-  return {
-      html: act.replace(/\{thing\}/g, thing)+'.',
-      unread: e[3] - last > 0,
-      url: thingurl,
-      pic: actpic,
-      is_photo: notifyType == 2
-    }
-});
-}
 
 
 
-
-
-//STUFF I WILL BE USING LATER
-
-
-function forceUpdate(){
-  var counturl = 'https://plus.google.com/u/'+(localStorage.auth_user||0)+'/_/n/guc';
-  if(global_inner_port){
-    global_inner_port.postMessage({action:'xhr', url: counturl});
-  }else{
-    var xhr = new XMLHttpRequest();
-    xhr.open('get',counturl,true);
-    xhr.onload = function(){
-      handleCount(xhr.responseText);
-    }
-    xhr.send();
-  }
-}
-
-
-function handleCount(text){
-  var num = JSON.parse(text.substr(4))[1];
-  drawIcon(num);
-  if(old_num < num) getNotifications(num);
-  old_num = num;
-}
 
